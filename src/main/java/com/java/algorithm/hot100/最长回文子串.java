0ce5bafa81014198280fa28a539d85e5f5e81646
qiaochao <qiaochao@icourt.cc>
2022-09-13 22:04:12 +0800

    feat : wrapper

---
+++ src/main/java/com/java/algorithm/hot100/最长回文子串.java
@@ -0,0 +1,26 @@
+package com.java.algorithm.hot100;
+/** 
+ * 所谓回文，就是正念和反念都一样
+ * 上海自来水来自海上
+ */
+public class 最长回文子串 {
+    public static void main(String[] args) {
+        
+    }
+    /** 有一本书叫做《代码随想录》，我最早起步的时候用得就是它
+     * 它介绍了两种解法
+     * 1.递推公式
+     * 2.双指针法，另外一题，无重复最长子串，用得也是双指针法
+     */
+    public static String longestPalindrome(String s) {
+
+
+
+
+
+        return "";
+
+    }
+
+
+}
