1b0e62604a374b900a89c2432646f6b224970f4d
qiaochao <qiaochao@icourt.cc>
2022-09-14 17:57:06 +0800

    feat : wrapper

--- src/main/java/com/java/algorithm/hot100/盛最多水的容器.java
+++ src/main/java/com/java/algorithm/hot100/盛最多水的容器.java
@@ -1,12 +1,19 @@
 package com.java.algorithm.hot100;
 
+
+/**
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+    找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+    返回容器可以储存的最大水量。
+    说明：你不能倾斜容器。
+ */
 public class 盛最多水的容器 {
     public static void main(String[] args) {
         
     }
     public static int maxArea(int[] height) {
 
-
+        //返回数据
         return 0;
     }
     
