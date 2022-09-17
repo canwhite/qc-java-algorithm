5512f07d0b4c96ba9508ffffd284250988ad9a96
qiaochao <qiaochao@icourt.cc>
2022-09-17 11:15:59 +0800

    feat : des

--- src/main/java/com/java/algorithm/hot100/删除链表的倒数第n个节点.java
+++ src/main/java/com/java/algorithm/hot100/删除链表的倒数第n个节点.java
@@ -26,7 +26,9 @@ public class 删除链表的倒数第n个节点 {
     //注意是倒数的链表
     public static ListNode removeNthFromEnd(ListNode head, int n) {
 
+        //有一个哑节点的概念
         ListNode pre=new ListNode(0,head);
+        //快慢指针
         ListNode fast=pre;
         ListNode slow=pre;
 
@@ -34,14 +36,15 @@ public class 删除链表的倒数第n个节点 {
         for (int i = 0; i < n +1; i++) {
             //找到next的位置
             fast=fast.next;
-            
         }
     
         //fast和slow是同一个指针，
         while (fast.next!=null){
+            //从正确位置往后走，走到有值的地方，相当于当前位没走
             fast=fast.next;     
             slow=slow.next;
         }
+        //再next就是对应位
         slow.next=slow.next.next;
         return pre.next;
     }   
