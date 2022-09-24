88a91bde3ba9afa94b59a90299155f153be59978
qiaochao <qiaochao@icourt.cc>
2022-09-24 16:43:19 +0800

    feat : wrapper

---
+++ src/main/java/com/java/algorithm/hot100/括号生成.java
@@ -0,0 +1,13 @@
+package com.java.algorithm.hot100;
+
+import java.util.List;
+
+public class 括号生成 {
+    public static void main(String[] args) {
+        
+    }
+    public  static List<String> generateParenthesis(int n) {
+
+    }
+    
+}
