70ee17f5b44e9972329f3ea481e0ca7bd4ef5e60
qiaochao <qiaochao@icourt.cc>
2022-10-19 16:39:23 +0800

    feat : 旋转图像

--- src/main/java/com/java/algorithm/hot100/旋转图像.java
+++ src/main/java/com/java/algorithm/hot100/旋转图像.java
@@ -1,4 +1,8 @@
 package com.java.algorithm.hot100;
+// import java.util.Arrays;
+
+import java.lang.System.Logger;
+import java.util.Arrays;
 
 public class 旋转图像 {
     public static void main(String[] args) {
@@ -11,10 +15,58 @@ public class 旋转图像 {
          * 输出：[[7,4,1],[8,5,2],[9,6,3]]
          */
 
+         /** 
+          * 转置 : 横向第一排转化为纵向第一排，依次类推
+          * 镜像 : 左边的到右边，或者右边的到左边
+          * 九十度 : 先转置，再左右
+          * 二百七十度 ：先转置，再上下
+          * 一百八十度 ：先上下，再左右
+          */
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
          
 
 
     }
+
+    public static void rotate(int[][] matrix) {
+        //先拿到size
+        int matrixSize = matrix.length;
+        
+        //先转置矩阵
+        for(int i = 0 ; i< matrixSize; i++){
+            //j<i等于是把左下角的内容转换到右上角
+            for(int j = 0 ; j < i; j++){
+                //交换
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //再左右镜像，将左边的换到右边
+        int left = 0;
+        int right = matrixSize-1;
+        //左右颠倒是第一列到第三列，left/right取j值
+        //上下颠倒是第三行到第一行，left/right取i值
+        //i是一个个的子树组
+        while(left < right){
+            for(int i = 0; i< matrixSize ; i++){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+            }
+            left ++;
+            right --;
+        }
+
+        for(int i = 0; i<matrixSize;i++){
+            Arrays.stream(matrix[i]).forEach(item->{
+                System.out.println(item);
+            });
+        }
+    }
+
+
 
     
 
