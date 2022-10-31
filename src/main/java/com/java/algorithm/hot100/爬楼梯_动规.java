b8322469c5824a8c5034a005a6dc7653552b3146
qiaochao <qiaochao@icourt.cc>
2022-10-31 21:57:45 +0800

    feat : wrapper

---
+++ src/main/java/com/java/algorithm/hot100/爬楼梯_动规.java
@@ -0,0 +1,23 @@
+package com.java.algorithm.hot100;
+
+public class 爬楼梯_动规 {
+    public static void main(String[] args) {
+        /** 
+        假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+        每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+
+        输入：n = 2
+        输出：2
+        解释：有两种方法可以爬到楼顶。
+        1. 1 阶 + 1 阶
+        2. 2 阶
+        */
+        
+    }
+    public static int climbStairs(int n) {
+        return 0;
+    }
+
+    
+}
