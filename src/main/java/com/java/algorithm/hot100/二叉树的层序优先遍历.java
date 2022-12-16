d7beb65b913356ffe33f88b2e24d7d5059b9eb7b
qiaochao <qiaochao@icourt.cc>
2022-12-16 15:21:49 +0800

    feat : bfs wrapper

--- src/main/java/com/java/algorithm/hot100/二叉树的层序优先遍历.java
+++ src/main/java/com/java/algorithm/hot100/二叉树的层序优先遍历.java
@@ -4,13 +4,25 @@ import java.util.List;
 
 public class 二叉树的层序优先遍历 {
     public static void main(String[] args) {
-        
+        //这个先想起来的的办法就是递归，BFS广度优先查询
+        /**
+         * 题目：
+         * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 
+         *（即逐层地，从左到右访问所有节点）。
+         * 输入：root = [3,9,20,null,null,15,7]
+         * 输出：[[3],[9,20],[15,7]]
+         */
+
 
 
         
     }
     
     public static  List<List<Integer>> levelOrder(TreeNode root) {
+        //来具体操作
+
+
+
 
         return null;
     }
