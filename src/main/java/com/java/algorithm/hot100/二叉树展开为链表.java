d4a9c1a5289046cb40ad7e48555a256949547369
qiaochao <qiaochao@icourt.cc>
2023-01-01 18:13:12 +0800

    feat : wrapper

---
+++ src/main/java/com/java/algorithm/hot100/二叉树展开为链表.java
@@ -0,0 +1,15 @@
+package com.java.algorithm.hot100;
+
+public class 二叉树展开为链表 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void flatten(TreeNode root) {
+
+        
+    }
+
+    
+}
